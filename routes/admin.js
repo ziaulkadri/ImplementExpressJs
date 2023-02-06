97f63c9d48d1508093f2dddb8b925f1ee41bd3e8
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-06 16:05:04 +0530

    added dynamic content using pug

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,8 @@ const products = [];
 router.get('/addProduct', (req, res, next) => {
 	//res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	// res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 router.post('/addProduct', (req, res, next) => {
